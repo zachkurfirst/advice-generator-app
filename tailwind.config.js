f77e4f1cccd325a890c627350b46442d051679e4
Zach Kurfirst <zachmk11@gmail.com>
2024-04-12 12:07:05 -0400

    set up basic HTML and styling to start

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,18 @@
 module.exports = {
   content: ["./public/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "light-cyan": "hsl(193, 38%, 86%)",
+        "neon-green": "hsl(150, 100%, 66%)",
+        "grayish-blue": "hsl(217, 19%, 38%)",
+        "dark-grayish-blue": "hsl(217, 19%, 24%)",
+        "dark-blue": "hsl(218, 23%, 16%)",
+      },
+    },
+    fontFamily: {
+      manrope: ["Manrope", "sans-serif"],
+    },
   },
   plugins: [],
-}
-
+};
