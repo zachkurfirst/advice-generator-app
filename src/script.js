73ac17a4f89a138ba978269dfe77b4588456724c
Zach Kurfirst <zachmk11@gmail.com>
2024-04-15 16:58:37 -0400

    fetch and render advice on load and button click

--- src/script.js
+++ src/script.js
@@ -1,12 +1,28 @@
-console.log("Is this thing on?");
-
 const URL = "https://api.adviceslip.com/advice";
+
+const adviceNumber = document.querySelector("span");
+const adviceQuote = document.querySelector("q");
+const fetchBtn = document.querySelector("button");
 
 const fetchAdvice = async () => {
   const response = await fetch(URL);
-  console.log(response);
-  const adviceData = await response.json();
-  console.log(adviceData);
+  const data = await response.json();
+  return data;
 };
 
-fetchAdvice();
+const renderAdvice = async (adviceSlip) => {
+  // destructure the object
+  const { id, advice } = adviceSlip;
+  adviceNumber.textContent = id;
+  adviceQuote.textContent = advice;
+};
+
+const generateAdvice = async () => {
+  const adviceData = await fetchAdvice();
+  const advice = adviceData.slip;
+  console.log(advice);
+  renderAdvice(advice);
+};
+
+fetchBtn.addEventListener("click", generateAdvice);
+document.addEventListener("DOMContentLoaded", generateAdvice);
